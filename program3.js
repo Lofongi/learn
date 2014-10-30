929db7db1a856ef0bdae3161150cf4f019574613
Ng Lofongi <aviastovvtf@gmail.com>
2014-10-30 17:33:31 +0200

    додав коментар розробника

--- program3.js
+++ program3.js
@@ -16,6 +16,18 @@ function logMyNumber() {
 }
 A(logMyNumber)
 // або fs.readFileSync(process.argv[2], 'utf8')
+/*
+
+ var fs = require('fs')
+ var file = process.argv[2]
+
+ fs.readFile(file, function (err, contents) {
+   // fs.readFile(file, 'utf8', callback) can also be used
+   var lines = contents.toString().split('\n').length - 1
+   console.log(lines)
+ })
+
+*/
 
 
 
